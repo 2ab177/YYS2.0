85fc2f509cb19f1a6b3203455c3247ad605934b4
lyf <332382@qq.com>
2019-08-20 20:04:22 +0800

    perfect project in 8.20

--- server/public/js/news.js
+++ server/public/js/news.js
@@ -26,4 +26,24 @@ $(function(){
     $(".nav_bg").removeClass("gf_bg");
     $(".gf_nav").removeClass("gf_enter");
   });
+  $.ajax({
+      url:"http://localhost:8080/news",
+      type:"get",
+      dataType:"json",
+      success:function(result){
+        var html = "";
+        for (var nw of result) {
+          html += `<div class="new_big">
+          <div class=" new_margin position-relative">
+            <a class="d-inline-block px-3 py-1 h5 new position-absolute mt-5 text-dark" href="javascript:;">新闻</a>
+            <div class="d-inline-block new_right  mt-5">
+              <h4><a class="text-dark" href="javascript:;">${nw.newtime} ${nw.ntitle}</a></h4>
+              <p><a class="text-secondary" href="javascript:;">${nw.ncontent}</a></p>
+            </div>
+          </div>
+        </div>`
+        }
+        $("#new_content").html(html);
+      }
+    });
 })
